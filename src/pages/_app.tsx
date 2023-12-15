b4982de01ecc9412b862b4ca122c72d8d33bfb38
Thomas Lillo <thomas.lillo@environicsanalytics.com>
2023-12-15 12:16:20 -0500

    MAKING IT REAL

--- src/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
-// this is the main entrypoint for the app - it wraps all the pages
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
