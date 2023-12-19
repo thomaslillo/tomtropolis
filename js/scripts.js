639df8204ee7f9a4e6e935b236dcc063765d1567
Thomas Lillo <thomas.lillo@environicsanalytics.com>
2023-12-19 14:56:27 -0500

    updating search function

--- js/scripts.js
+++ js/scripts.js
@@ -11,37 +11,39 @@ searchButton.addEventListener("click", () => {
   }
 });
 
-function searchMonster(monsterName) {
+async function searchMonster(monsterName) {
+  // loading message
   monsterResult.innerHTML = "Searching...";
+  // search for the monster
+  const monsterNameFormatted = monsterName.toLowerCase().replace(" ", "-");
 
-  // switch this to something else
-  axios
-    .get(`https://www.dnd5eapi.co/api/monsters`)
-    .then((response) => {
-      const monsters = response.data.results;
-      const matchedMonster = monsters.find(
-        (monster) => monster.name.toLowerCase() === monsterName
-      );
-
-      if (matchedMonster) {
-        axios
-          .get(matchedMonster.url)
-          .then((monsterResponse) => {
-            const monsterData = monsterResponse.data;
-            monsterResult.innerHTML = `
-              <h2>${monsterData.name}</h2>
-              <p><strong>Index:</strong> ${monsterData.index}</p>
-              <!-- You can display more monster details here -->
-            `;
-          })
-          .catch((error) => {
-            monsterResult.innerHTML = "Error fetching monster details.";
-          });
-      } else {
-        monsterResult.innerHTML = "Monster not found.";
-      }
-    })
-    .catch((error) => {
-      monsterResult.innerHTML = "Error fetching monsters.";
-    });
+  try {
+    const response = await fetch(
+      `https://www.dnd5eapi.co/api/monsters/${monsterNameFormatted}`
+    );
+    if (!response.ok) {
+      throw new Error(`${response.status} ${response.statusText}`);
+    }
+    const data = await response.json();
+    console.log(data);
+    // display the moster
+    // TODO make this more dynamic and a seperate function
+    monsterResult.innerHTML = `
+    <div class="m-2 font-mono">
+      <h1>${data.name}</h1>
+      <p><strong>Type:</strong> ${data.type}</p>
+      ${data.subtype ? `<p><strong>Subtype:</strong> ${data.subtype}</p>` : ''}
+      <p><strong>Size:</strong> ${data.size}</p>
+      <p><strong>Alignment:</strong> ${data.alignment}</p>
+      <p><strong>Health:</strong> ${data.hit_points}</p>
+      <p><strong>Languages:</strong> ${data.languages}</p>
+      <p><strong>Challenge Rating:</strong> ${data.challenge_rating}</p>
+      ${ data.image ? `<img src="https://www.dnd5eapi.co${data.image}">` : '' }      
+    </div>
+    `;    
+  } catch (error) { 
+    console.log(error);
+    monsterResult.innerHTML = `Monster not found, please try again with a 
+    different monster or different name for the same moster.`;
+  }
 }
